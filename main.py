494409561b0c3adce85054bf16c2c94e76078c7f
Awokima <a.apenitis@gmail.com>
2024-11-08 23:48:48 +0200

    Added scraping script, updated main, added .gitignore

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from scraping.scraper import scrape_user_data
-from processing.data_processing import process_data
-from analysis.sentiment_analysis import analyze_sentiment
-from visualization.visualization import create_visualizations
-from reports.report_generator import generate_report
+import time
+from scraping.scraper import ThreadsScraper
+#from processing.data_processing import process_data
+#from analysis.sentiment_analysis import analyze_sentiment
+#from visualization.visualization import create_visualizations
+#from reports.report_generator import generate_report
 
 #call each module
 def main():
-    scraper = ThreadsScraper("https://threads.net")
-    #implement other modules
+    base_url = "https://www.threads.net"
+     # Create an instance of ThreadsScraper
+    scraper = ThreadsScraper(base_url)
+
+    # Specify the username you want to fetch the profile for
+    usernames = ["jonrettinger","ktla5news"]  # Example usernames
+
+    for username in usernames:
+        print(f"\nFetching profile for: {username}")
+        profile_data = scraper.fetch_profile(username)
+        
+        if profile_data:
+            print("Profile Data:", profile_data)
+            if profile_data['bio'] == "Bio not found" and 'error_details' in profile_data:
+                print("Debug Info:", profile_data['error_details'])
+        else:
+            print("No data retrieved.")
+        
+        # Add delay between requests to be respectful
+        time.sleep(2)
 
 
 if __name__ == "__main__":
--- scraping/scraper.py
+++ scraping/scraper.py
@@ -4,3 +4,21 @@ from bs4 import BeautifulSoup
 class ThreadsScraper:
     def __init__(self, base_url):
         self.base_url = base_url
+
+    def fetch_profile(self, username):
+        url = f"{self.base_url}/{username}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            profile_data = {'username': username}
+            # Parse bio information within the specific div and span structure
+            biocontainer = soup.find('div', class_='x17zef60')
+            if biocontainer:
+                bio_text = biocontainer.find('span', class_='x1lliihq x1plvlek xryxfnj x1n2onr6 x1ji0vk5 x18bv5gf x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye xjohtrz xo1l8bm xp07o12 x1yc453h')
+                profile_data['bio'] = bio_text.get_text(strip=True)
+            else:
+                profile_data['bio'] = "Bio not found"
+                return profile_data
+        else:
+            print(f"Failed to retrieve data for {username}")
+            return None
